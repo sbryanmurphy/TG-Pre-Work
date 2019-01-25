4d886cf5df0a9eec7791737c0e55a9e585397cdc
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 16:43:35 -0800

    JavaScript - Introduction to JavaScript - Properties

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Properties.js
@@ -0,0 +1,3 @@
+console.log('Teaching the world how to code'.length);
+
+// learned that strings have properties. the first one we learn about is length, which tells us how many characters are in the string
