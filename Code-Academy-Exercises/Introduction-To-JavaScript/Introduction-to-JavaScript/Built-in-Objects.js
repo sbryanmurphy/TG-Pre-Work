5028ab00cf63197aeed404378604ec133b3df39e
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 16:47:29 -0800

    JavaScript - Introduction to JavaScript - Built-In Objects

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,5 @@
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+// learned about built-in objects. first objects we learned about are the Math and Number objects. Objects are rad
