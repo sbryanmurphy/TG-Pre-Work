3275306db47146c315cd7c2d336d715e8eec5755
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 22:49:15 -0800

    JavaScript - Introduction to JavaScript - The Switch Keyword

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/The-switch-keyword.js
@@ -0,0 +1,17 @@
+let athleteFinalPosition = 'second place';
+
+switch (athleteFinalPosition){
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default: console.log('No medal awarded.');
+    break;
+}
+
+// learned about the switch statement which is an easier way to write a whole bunch of else if statements
