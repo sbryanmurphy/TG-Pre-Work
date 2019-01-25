4c442c5597ab862f54e12fc373f2232a7233da9e
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 17:14:19 -0800

    JavaScript - Conditional Statements - Comparison Operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -0,0 +1,8 @@
+let hungerLevel = 7;
+if (hungerLevel>7) {
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!');
+}
+
+//learned about comparison operators which are like arithmetic operators except these ones compare values in terms of greater than, less than, equal etc...  
