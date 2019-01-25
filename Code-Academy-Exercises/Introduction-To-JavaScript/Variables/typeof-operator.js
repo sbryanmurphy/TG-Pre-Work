70c32627d0e5fe3eb5f7993d3af4011c946358bb
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 17:06:40 -0800

    JavaScript - Variables - typeof Operator

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/typeof-operator.js
@@ -0,0 +1,5 @@
+let newVariable = 1;
+console.log(typeof newVariable);
+console.log(typeof newVariable);
+
+//learned about the typeof operator, which checks the value to its right and passes back which data type it is
