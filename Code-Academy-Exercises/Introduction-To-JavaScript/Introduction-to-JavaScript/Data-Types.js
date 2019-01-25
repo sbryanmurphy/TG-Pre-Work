1c5d952cf15d04cb130afe1ed3a7e130c81d973e
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 16:38:55 -0800

    JavaScript - Introduction to JavaScript - Data Types

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Data-Types.js
@@ -0,0 +1,6 @@
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+// learned about the different data types used in JavaScript
