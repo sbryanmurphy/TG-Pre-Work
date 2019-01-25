9b9fb4771c679baab9c884457cd214d29ba7be5c
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-25 10:31:21 -0800

    JavaScript - Functions - Calling a Function

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Calling-a-Function.js
@@ -0,0 +1,8 @@
+function sayThanks(){
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+sayThanks();
+sayThanks();
+sayThanks();
+
+//learned how to call a function by typing out the name of the function followed by a parentheses
