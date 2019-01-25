83a007a07498f65bc569372b9413307512f16f3c
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 17:25:29 -0800

    JavaScript - Conditional Statements - Ternary Operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,10 @@
+let tool = '';
+
+tool = 'marker';
+
+// Use short circuit evaluation to assign writingUtensil variable below:
+let writingUtensil = tool || 'marker';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+//started learning about truthy and falsy statements. To be honest, I'm having trouble grasping the difference between truthy/falsy and true/false...
