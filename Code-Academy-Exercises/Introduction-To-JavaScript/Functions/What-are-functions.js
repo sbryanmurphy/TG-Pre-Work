406e12be035a54ee52273f428a167ae948293f9a
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 22:56:02 -0800

    JavaScript - Functions - What Are Functions

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/What-are-functions.js
@@ -0,0 +1,1 @@
+//read the overview of the purpose of a function and watched a fun lil gif of a visual representation of a function called addOneSide() and shows what it would do to different inputs
