d250a07a9291815c2acda045d55a991454d86a13
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 22:57:45 -0800

    JavaScript - Functions - Function Declarations

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Declaration.js
@@ -0,0 +1,9 @@
+function getReminder(){
+  console.log('Water the plants.')
+}
+
+function greetInSpanish(){
+  console.log('Buenas Tardes.')
+}
+
+//learned more about the function and learned the anatomy of the function declaration
