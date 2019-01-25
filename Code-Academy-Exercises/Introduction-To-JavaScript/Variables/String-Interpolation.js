612b7bbe39924a07b23c45930c0064769e375a0a
Shawn Murphy <shawn.sbm@gmail.com>
2019-01-24 17:04:53 -0800

    JavaScript - Variables - String Interpolation

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,5 @@
+var myName = 'Shawn';
+var myCity = 'Atlanta';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+// learned about string interpolation using template literals and placeholders. 
